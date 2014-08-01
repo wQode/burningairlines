8c4c64f36155ea60396d4d936aac9912db20a2a9
WQU <wqucodebreaker@gmail.com>
2014-08-01 10:25:21 +1000

    reservations crud

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,32 @@
+class ReseravationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+      respond_to do |format|
+        format.html {}
+        format.json{ render :json => @reservations }
+      end
+  end
+
+  def show
+    @reservation = Reservation.find params[:id]
+  end
+
+  def create
+    @reservation = Reservation.create reservation_params
+    render :json => @reservation
+  end
+
+  def update
+    @reservation = Reservation.find params[:id]
+    @reservation.update reservation_params
+    render :json => @reservation
+  end
+
+
+  private
+    def reseravation_params
+      params.require(:reservation).permit(:user_id, :flight_id)
+    end
+  end
+  
+end
